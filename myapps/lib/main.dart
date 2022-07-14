13ec3aa0c43d37bee0b5ccbd4a5ce58450e59c27
tustiantiifany <tustiantifany@gmail.com>
2022-07-14 22:03:15 +0700

    codelab 2

--- myapps/lib/main.dart
+++ myapps/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapps/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,84 +16,6 @@ class MyApp extends StatelessWidget {
       title: "Wisata Bandung",
       theme: ThemeData(),
       home: DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 16),
-              child: Text(
-                'Farmhouse Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.calendar_today),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('Open Everyday')
-                    ],
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.access_time_outlined),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('09.00 - 20.00')
-                    ],
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.monetization_on_rounded),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text('Rp.25.000')
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
